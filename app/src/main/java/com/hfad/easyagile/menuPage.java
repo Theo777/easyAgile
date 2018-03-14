d58437cd6ac09b95ed28ef2009a4fb06818bff52
Theo777 <theoslepski@yahoo.com>
2018-03-14 18:20:11 -0400

    firstattemp
    wake up neil

--- app/src/main/java/com/hfad/easyagile/menuPage.java
+++ app/src/main/java/com/hfad/easyagile/menuPage.java
@@ -3,16 +3,11 @@ package com.hfad.easyagile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class messageBoard extends AppCompatActivity {
+public class menuPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_board);
+        setContentView(R.layout.activity_menu_page);
     }
-    //
-    /////
-    //
-
-    //
 }
