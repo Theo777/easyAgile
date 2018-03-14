7eeecba71aad1a68355e989797969ec0553bd72f
Theo777 <theoslepski@yahoo.com>
2018-03-14 18:43:21 -0400

    I do not think neil will work on this but I am commiting anyway

--- app/src/main/java/com/hfad/easyagile/menuPage.java
+++ app/src/main/java/com/hfad/easyagile/menuPage.java
@@ -1,7 +1,13 @@
 package com.hfad.easyagile;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class menuPage extends AppCompatActivity {
 
