19210d585b2a7ea673a9a174c8f5da2ada1eb4af
Neil Fitzgerald <Neil Fitzgerald>
2018-04-02 18:58:25 -0400

    making changes for merge

--- app/src/main/java/com/hfad/easyagile/homePage.java
+++ app/src/main/java/com/hfad/easyagile/homePage.java
@@ -5,7 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class homePage extends AppCompatActivity {
+
+public class homePage extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +15,8 @@ public class homePage extends AppCompatActivity {
     }
 
     public void onsubmitButton(View view){
+
+
 
     Intent intent = new Intent(this,menuPage.class);
 
